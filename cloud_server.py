934a3c987083f0306e79913784ae201e2bd1653a
Reza Tavasoli <jfk998@gmail.com>
2020-11-15 18:30:01 +0330

    Add partially working buttons

--- cloud_server.py
+++ cloud_server.py
@@ -1,54 +1,111 @@
 from flask import Flask, request, render_template, jsonify, Response, abort, json
+from subprocess import PIPE, run
 import subprocess
 
 app = Flask(__name__)
+
+app.config["CACHE_TYPE"] = "null"
 
 
 @app.route("/")
 def home():
     # return "Hello, World!"
-    return render_template('dashboard.html')
+    return render_template('dash.html')
+
+
+@app.route('/vm_on')
+def vm_on():
+    # subprocess.call(["VBoxManage", "controlvm", "vm1", "poweroff"])
+    command = ["VBoxManage", "startvm", "vm1"]
+    result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+    print(result.returncode, result.stdout, result.stderr)
+    # subprocess.call(["VBoxManage", "startvm", "vm1"])
+    return "nothing"
 
 
 # background process happening without any refreshing
-@app.route('/vm1_on')
-def vm_on():
-    subprocess.call(["VBoxManage", "startvm", "vm1"])
-    return None
-
-
+# @app.route('/vm1_on')
+# def vm_on():
+#     print('booyah')
+#     # a = int(request.args.get('val1'))
+#     subprocess.call(["VBoxManage", "controlvm", "vm1", "poweroff"])
+#     return "nothing"
+#
+#
 @app.route('/vm1_off')
 def vm_off():
-    subprocess.call(["VBoxManage", "controlvm", "vm1", "poweroff"])
-    return None
+    print('koonah')
+    # subprocess.call(["VBoxManage", "controlvm", "vm1", "poweroff"])
+    command = ["VBoxManage", "controlvm", "vm1", "poweroff"]
+    result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+    print(result.returncode, result.stdout, result.stderr)
+    return "nothing"
+
 
 @app.route('/vm_cpu_memory')
 def vm_change_cpu_memory():
-    subprocess.call(["VBoxManage", "modifyvm", "vm1", "--memory", "4000"])
-    subprocess.call(["VBoxManage", "modifyvm", "vm1", "--cpus", "3"])
-    return None
+    print("came here")
+    a = int(request.args.get('cpu'))
+    b = int(request.args.get('memory'))
+    command1 = ["VBoxManage", "modifyvm", "vm1", "--memory", str(b)]
+    result1 = run(command1, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+    print(result1.returncode, result1.stdout, result1.stderr)
 
-@app.route('/vm_delete')
-def vm_delete():
-    subprocess.call(["VBoxManage", "unregister", "vm1", "-delete"])
-    return None
+    command2 = ["VBoxManage", "modifyvm", "vm1", "--cpus", str(a)]
+    result2 = run(command2, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+    print(result2.returncode, result2.stdout, result2.stderr)
+    # subprocess.call(["VBoxManage", "modifyvm", "vm1", "--memory", str(b)])
+    # subprocess.call(["VBoxManage", "modifyvm", "vm1", "--cpus", str(a)])
+    return jsonify({"result": a + b})
 
+
+@app.route('/vm_command')
+def vm_command():
+    commands = request.args.get('commands')
+    commands = commands.split()
+    print(commands)
+    cmd_list = ["VBoxManage", "guestcontrol", "vm1", "--username", "reza", "--password", "19422010", "run", "--exe",
+                f"/bin/{commands[0]}", f"{commands[0]}"]
+    if len(commands) > 1:
+        cmd_list += commands[1:]
+    # subprocess.call(cmd_list)
+    # command = ["VBoxManage", "controlvm", "vm1", "poweroff"]
+    result = run(cmd_list, stdout=PIPE, stderr=PIPE, universal_newlines=True)
+    print(result.returncode, result.stdout, result.stderr)
+    # subprocess.call(["VBoxManage", "guestcontrol", "vm1", "----username", str(b), "--password", str(a), ])
+    # subprocess.call(["VBoxManage", "modifyvm", "vm1", "--cpus", str(a)])
+    # return jsonify({"result": a + b})
+    return jsonify({"result": result.stdout})
+
+
+#
+# @app.route('/vm_delete')
+# def vm_delete():
+#     print('delete')
+#     subprocess.call(["VBoxManage", "unregistervm", "vm33", "-delete"])
+#     return None
+#
+# @app.route('/vm_clone')
+# def vm_clone():
+#     print("clone kardi?????")
+#     subprocess.call(["VBoxManage", "clonevm", "vm1", "--register"])
+#     print("clone kardi?????")
+#     return None
+#
 @app.route('/calculate_result')
 def calculate_result():
-    a = int(request.args.get('val1'))
-    b = int(request.args.get('val2'))
-    # return jsonify({"result": a + b})
-    response = app.response_class(
-        response=jsonify({"result": a + b}),
-        status=403,
-        mimetype='application/json'
-    )
-    return response
+    print("came here")
+    a = int(request.args.get('cpu'))
+    # subprocess.call(["VBoxManage", "startvm", "vm1"])
+    b = int(request.args.get('memory'))
+    return jsonify({"result": a + b})
 
 
-@app.route("/salvador")
-def salvador():
-    return "Hello, Salvador"
+#
+#
+# @app.route("/salvador")
+# def salvador():
+#     return "Hello, Salvador"
 
 
 if __name__ == "__main__":
--- static/main.js
+++ static/main.js
@@ -1,17 +1,161 @@
 $(document).ready(function () {
 
-    $('.wrapper').on('click', '.get_result', function () {
-        $("#btnAddProfile").prop('value', 'Save');
-        var val1 = $("#input_A").val();
-        var val2 = $("#input_B").val();
+
+    $('#vm1wrapper').on('click', '#NCMMSend1', function () {
+        console.log("333")
+        // $("#NCMMSend1").prop('value', 'Save');
+        var cpu = $("#NoC1").val();
+        var memory = $("#MMS1").val();
         $.ajax({
-            url: "/calculate_result",
+            url: "/vm_cpu_memory",
             type: "get",
-            data: {val1: val1, val2: val2},
+            data: {cpu: cpu, memory: memory},
             success: function (response) {
                 $(".result").html('<p>' + response.result.toString() + '</p>');
-                $(".get_result").html('Sffffffave');
+                // $(".get_result").html('Sffffffave');
             },
         })
     });
+
+    $('#vm1cmdwrapper').on('click', '#MessageSend1', function () {
+        var commands = $("#Message1").val();
+        console.log(commands)
+        $.ajax({
+            url: "/vm_command",
+            type: "get",
+            data: {commands: commands},
+            success: function (response) {
+                $(".resultCmd").html('<p>' + response.result.toString() + '</p>');
+            },
+        })
+    });
+
+    // $('.wrapper').on('click', '.get_result', function () {
+    //     console.log("333")
+    //     $("#btnAddProfile").prop('value', 'Save');
+    //     var val1 = $("#input_A").val();
+    //     var val2 = $("#input_B").val();
+    //     $.ajax({
+    //         url: "/calculate_result",
+    //         type: "get",
+    //         data: {val1: val1, val2: val2},
+    //         success: function (response) {
+    //             console.log("mmcvbxvb")
+    //             $(".result").html('<p>' + response.result.toString() + '</p>');
+    //             $(".get_result").html('Sffffffave');
+    //         },
+    //     })
+    // });
+
+    // $("a#delete1click").click(function (e) {
+    //     e.preventDefault();
+    //     $.ajax({
+    //         url: "/vm1_on",
+    //         type: "get",
+    //         // data: {val1: val1},
+    //         success: function (response) {
+    //             console.log("delete success")
+    //         },
+    //     })
+    // });
+    $("a#clone1click").click(function (e) {
+        e.preventDefault();
+        console.log("clone1");
+        var val1 = 999;
+        $.ajax({
+            url: "/vm_on",
+            type: "get",
+            // data: {val1: val1},
+            success: function (response) {
+                console.log("clone success")
+            },
+        })
+    });
+        $("a#delete1click").click(function (e) {
+        e.preventDefault();
+        console.log("clone1");
+        var val1 = 999;
+        $.ajax({
+            url: "/vm1_off",
+            type: "get",
+            // data: {val1: val1},
+            success: function (response) {
+                console.log("clone success")
+            },
+        })
+    });
+
+
+    // $('a#clone1click').on('click', function (e) {
+    //     e.preventDefault()
+    //     $.getJSON('/ahmad',
+    //         function (data) {
+    //
+    //         });
+    //     return false;
+    // });
+
+    // $('a#clone1click').on('click', function (e) {
+    //     e.preventDefault()
+    //     $.getJSON('/ahmad',
+    //         function (data) {
+    //
+    //         });
+    //     return false;
+    // });
+
+
+    // $('#myonoffswitch1').on('change',function() {
+    //         e.preventDefault();
+    //         if(this.checked){
+    //         alert("hiiiiii");
+    //
+    //         }
+    //   });
+
+    // $('.wrappp').on('click', '#emailNotification', function () {
+    //     print('here4')
+    //     alert($(this).attr('id'));  //-->this will alert id of checked checkbox.
+    //     if (this.checked) {
+    //         $.ajax({
+    //             type: "get",
+    //             url: '/calculate_result',
+    //             data: $(this).attr('id'), //--> send id of checked checkbox on other page
+    //             success: function (data) {
+    //                 alert('it worked');
+    //                 print('here1')
+    //                 alert(data);
+    //                 $('#container').html(data);
+    //             },
+    //             error: function () {
+    //                 alert('it broke');
+    //                 print('here2')
+    //             },
+    //             complete: function () {
+    //                 alert('it completed');
+    //                 print('here3')
+    //             }
+    //         });
+    //
+    //     }
+    // });
+
+    // $("#myonoffswitch1click").click(function() {
+    //     e.preventDefault();
+    //     alert("hi");
+    //     // alert($(this).attr('id'));  //-->this will alert id of checked checkbox.
+    //     if ($("#myonoffswitch1").checked) {
+    //         $.ajax({
+    //             type: "post",
+    //             url: '/vm_on',
+    //             // data: $(this).attr('id'), //--> send id of checked checkbox on other page
+    //             success: function (data) {
+    //
+    //             }
+    //         });
+    //
+    //     }
+    // });
+
+
 });
